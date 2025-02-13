95fa00c66f0a7f637edb864d0f277dcbac2ca670
Samwise-B <sam.burcher99@gmail.com>
2025-02-13 16:52:59 +0000

    (WIP) repo exception handling

--- src/main/java/com/weatherapp/myweatherapp/repository/VisualcrossingRepository.java
+++ src/main/java/com/weatherapp/myweatherapp/repository/VisualcrossingRepository.java
@@ -2,7 +2,14 @@ package com.weatherapp.myweatherapp.repository;
 
 import com.weatherapp.myweatherapp.model.CityInfo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Repository
@@ -16,6 +23,30 @@ public class VisualcrossingRepository {
   public CityInfo getByCity(String city) {
     String uri = url + "timeline/" + city + "?key=" + key;
     RestTemplate restTemplate = new RestTemplate();
+
+    try {
+      ResponseEntity<CityInfo> response = restTemplate.getForEntity(uri, CityInfo.class);
+
+      if (response.getStatusCode() == HttpStatus.OK) {
+        return response.getBody();
+      } else if (response.getStatusCode() == HttpStatus.BAD_REQUEST) {
+        throw new RuntimeException("Bad Request: Invalid City Name");
+      } else if (response.getStatusCode() == HttpStatus.TOO_MANY_REQUESTS) {
+        throw new RuntimeException("Too Many Requests: Rate Limit exceeded.");
+      } else if (response.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR) {
+        throw new RuntimeException("Server Error: The weather service is down. Please try again.");
+      } else {
+        throw new RuntimeException("Unexpected Error:" + response.getStatusCode());
+      }
+    } catch (HttpClientErrorException e) { // 4xx errors
+      System.err.println("Client Error: " + e.getStatusCode() + " - " + e.getResponseBodyAsString());
+    } catch (HttpServerErrorException e) { // 5xx errors
+      System.err.println("Server Error: " + e.getStatusCode() + " - " + e.getResponseBodyAsString());
+    } catch (ResourceAccessException e) { // Timeouts / Connection issues
+      System.err.println("Timeout or Connection Error: " + e.getMessage());
+    } catch (RestClientException e) { // General RestTemplate errors
+      System.err.println("Request Failed: " + e.getMessage());
+    }
     return restTemplate.getForObject(uri, CityInfo.class);
 
   }
